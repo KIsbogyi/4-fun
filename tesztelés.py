4cb6092e6deb515327f296bd2b6df9ab9f6897d2
KIsbogyi <55801726+KIsbogyi@users.noreply.github.com>
2020-01-09 15:50:42 +0100

    Add files via upload

---
+++ tesztelés.py
@@ -0,0 +1,9 @@
+from os.path import expanduser
+import os
+
+
+home= expanduser('~')
+hely=home+'\Appdata\Roaming\Microsoft\Windows\Start Menu\Programs\helo.txt'   #\a hangszórót akarja megszólaltatni
+print(hely)
+os.startfile(hely)
+
