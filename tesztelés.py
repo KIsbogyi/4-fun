9a4f948b38a1f979d69e4620bdb3f6da256dcf12
KIsbogyi <55801726+KIsbogyi@users.noreply.github.com>
2020-01-09 17:47:20 +0100

    Update tesztelés.py

--- tesztelés.py
+++ tesztelés.py
@@ -6,4 +6,5 @@ home= expanduser('~')
 hely=home+'\Appdata\Roaming\Microsoft\Windows\Start Menu\Programs\helo.txt'   #\a hangszórót akarja megszólaltatni
 print(hely)
 os.startfile(hely)
+os.write(hely,#mit írsz be)
 
